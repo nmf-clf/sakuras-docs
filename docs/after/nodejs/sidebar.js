f5282e5c4eb0aa871043b0086b0f4907d1df86a4
niumengfei <870424431@qq.com>
2022-10-17 22:50:45 +0800

    nodejs学习

--- docs/after/nodejs/sidebar.js
+++ docs/after/nodejs/sidebar.js
@@ -2,14 +2,15 @@
  * @Author: niumengfei
  * @Date: 2022-10-14 09:46:36
  * @LastEditors: niumengfei
- * @LastEditTime: 2022-10-14 09:51:38
+ * @LastEditTime: 2022-10-17 20:18:48
  */
 module.exports = [
     {
         text: "Nodejs",
         children: [
             "/after/nodejs/README.md", 
-            "/after/nodejs/01-基础.md"
+            "/after/nodejs/01-基础.md",
+            "/after/nodejs/02-高级.md"
         ],
     },
 ]
